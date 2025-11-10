d6e24cc3178c1741e9f95badcd6b80e025226745
charcasbeos <tranthianhthu0711@gmail.com>
2025-11-10 11:24:58 +0700

    chore: initialize user service with Spring Boot

---
+++ tripmate-user-service/src/main/java/com/example/tripmate_user_service/TripmateUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripmate_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmateUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmateUserServiceApplication.class, args);
+	}
+
+}

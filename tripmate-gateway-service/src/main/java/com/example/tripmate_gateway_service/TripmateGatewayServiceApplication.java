f726fcf34c2412299584d6d13fbdfcc2f38ce0cc
charcasbeos <tranthianhthu0711@gmail.com>
2025-11-17 14:26:11 +0700

    chore: initialize gateway service

---
+++ tripmate-gateway-service/src/main/java/com/example/tripmate_gateway_service/TripmateGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripmate_gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmateGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmateGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ tripmate-gateway-service/src/test/java/com/example/tripmate_gateway_service/TripmateGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripmate_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripmateGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

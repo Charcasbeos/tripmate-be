9fff888bf103f85db5bb3043b610a541ec48266f
charcasbeos <tranthianhthu0711@gmail.com>
2025-11-17 14:25:00 +0700

    chore: change data type for user id

--- tripmate-user-service/src/main/java/com/example/tripmate_user_service/entity/User.java
+++ tripmate-user-service/src/main/java/com/example/tripmate_user_service/entity/User.java
@@ -27,7 +27,7 @@ import lombok.Setter;
 public abstract  class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
     private String username;
     private String password;
     private String avatar;
